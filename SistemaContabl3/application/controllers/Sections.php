739aa7aee49f2f85bf99ba6ae68b91e1d4c84460
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-02-07 22:28:36 +0000

    rutas de sections y carga de vistas

---
+++ SistemaContabl3/application/controllers/Sections.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sections extends CI_Controller {
+    function soporte(){
+        $this->load->view('header');
+        $this->load->view('sections/soporte');
+        $this->load->view('footer');
+    }
+      function faq(){
+        $this->load->view('header');
+        $this->load->view('sections/faq');
+        $this->load->view('footer');
+    }
+      function contacto(){
+        $this->load->view('header');
+        $this->load->view('sections/contacto');
+        $this->load->view('footer');
+    }
+    
+      function quiensomos(){
+        $this->load->view('header');
+        $this->load->view('sections/quiensomos');
+        $this->load->view('footer');
+    }
+    
+  
+}
+?>
