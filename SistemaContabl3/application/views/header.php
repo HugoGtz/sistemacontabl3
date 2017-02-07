8c87f56e6be68bb90bba0047a588d6a9134fcb3c
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-02-07 21:56:02 +0000

    Pagina principal template.

---
+++ SistemaContabl3/application/views/header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistemacontabl3.com</title>
+	<script
+  src="https://code.jquery.com/jquery-3.1.1.js"
+  integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+  crossorigin="anonymous"></script>
+ 
+
+	 <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<style type="text/css">
+  html{
+  	font-family: 'Roboto', sans-serif;
+  }
+</style>
+<body>
+    <div class="navbar-fixed">
+ <nav style="background:#fff" role="navigation">
+    <div class="nav-wrapper container ">
+      <a id="logo-container" href="#" class="brand-logo black-text">Logo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a class="black-text" href="#">Navbar Link</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+</div>
+
+
+<script type="text/javascript" >
+    $('.button-collapse').sideNav();
+    
+</script>
+
+
+ 
